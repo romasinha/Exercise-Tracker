2d38eee828883b2a81dce41cf36d86ae8242201b
romasinha <sinha.roma99@gmail.com>
2020-10-21 14:19:14 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +20,13 @@ const connection = mongoose.connection;
 connection.once('open', ()=>{
     console.log("MongoDB database connection established successfully")
 })
-app.listen(port, ()=>{
-    console.log(`The app is listening on ${port}`);
-});
+
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', usersRouter);
+
+app.listen(3000, ()=>{
+    console.log('server started')
+})
